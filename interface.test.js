f91b3a9bc0e148b8c5ca893ba3dacc3df905bf4c
Joe Delfino <jdelfino@gmail.com>
2024-03-13 14:53:41 +0000

    break test up into smaller cases

--- interface.test.js
+++ interface.test.js
@@ -1,6 +1,6 @@
 const ShoeSize = require('./interface');
 
-it('should convert shoe sizes correctly and handle invalid shoe sizes', () => {
+it('should initialize correctly from a US shoe size', () => {
     let shoeSize = ShoeSize.fromUsSize(9);
     expect(shoeSize.usSize()).toBe(9);
     expect(shoeSize.ukSize()).toBe(7);
@@ -8,18 +8,25 @@ it('should convert shoe sizes correctly and handle invalid shoe sizes', () => {
     shoeSize = ShoeSize.fromUsSize(10);
     expect(shoeSize.usSize()).toBe(10);
     expect(shoeSize.ukSize()).toBe(8);
+});
 
-    shoeSize = ShoeSize.fromUkSize(7);
+it('should initialize correctly from a UK shoe size', () => {
+    let shoeSize = ShoeSize.fromUkSize(7);
     expect(shoeSize.usSize()).toBe(9);
     expect(shoeSize.ukSize()).toBe(7);
 
     shoeSize = ShoeSize.fromUkSize(8);
     expect(shoeSize.usSize()).toBe(10);
     expect(shoeSize.ukSize()).toBe(8);
+});
 
+it('should throw an error when initialized with a negative number', () => {
     expect(() => { ShoeSize.fromUsSize(-1) }).toThrow();
-    expect(() => { ShoeSize.fromUsSize('17') }).toThrow();
-
     expect(() => { ShoeSize.fromUkSize(-1) }).toThrow();
+
+});
+
+it('should throw an error when initialized with a string', () => {
+    expect(() => { ShoeSize.fromUsSize('17') }).toThrow();
     expect(() => { ShoeSize.fromUkSize('17') }).toThrow();
 });
